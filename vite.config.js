2eb248c0ea9a0587cc7028cedaac8bee20872ceb
Andrew Welch <andrew@keluli.local>
2021-10-31 17:04:39 -0400

    Exclude deps that fail to resolve in optimizeDeps

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,10 @@ export default ({ command }) => ({
     build: {
         emptyOutDir: true,
     },
+    // These deps fail to resolve for some reason
+    optimizeDeps: {
+        exclude: ['fs-dir/test', 'regex/test', 'dep', 'foo', '@virtual-file']
+    },
     plugins: [
     ],
     server: {
